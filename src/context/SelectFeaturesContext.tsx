a2d8c41f0aa9484e0476ca9e4c10842ed47a7547
gVillacura <geraldinevillacuraruz@gmail.com>
2021-10-12 12:02:48 -0300

    feat: add news context

---
+++ src/context/SelectFeaturesContext.tsx
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const SelectFeatureContext = createContext(undefined);
+
+export default SelectFeatureContext;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
 import { Box } from "@chakra-ui/react";
+import { useState } from "react";
 
 import Simulator from "components/simulator/index";
+import SelectFeatureContext from "context/SelectFeaturesContext";
+
+interface StatesProps {
+  label: string;
+}
 
 const Home = () => {
+  const [states, setStates] = useState<string>("alabama");
+
   return (
-    <Box>
-      <Simulator />
-    </Box>
+    <SelectFeatureContext.Provider value={{ states, setStates }}>
+      <Box>
+        <Simulator />
+      </Box>
+    </SelectFeatureContext.Provider>
   );
 };
 
