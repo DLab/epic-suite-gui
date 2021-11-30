58d989189dd3149151bc256ec780851df5ca925a
Pablo Morales Pinto <70617170+ElpolMorris@users.noreply.github.com>
2021-11-30 18:06:25 -0300

    feat: page 404 and favicon

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,42 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Flex, Text, Center, Link, Icon } from "@chakra-ui/react";
+
+import MicroscopeIcon from "components/icons/MicroscopeIcon";
+import WomanMedicalIcon from "components/icons/WomanMedicalIcon";
 
 const Page404 = () => {
   return (
     <>
-      <Box marginY={4}>
-        <Heading textAlign="center">Page not Found!</Heading>
-      </Box>
+      <Flex
+        direction="column"
+        marginY={4}
+        h="79vh"
+        align="center"
+        justify="center"
+      >
+        <Icon
+          as={WomanMedicalIcon}
+          w={250}
+          h={250}
+          aria-label="Woman medical Icon"
+          fill="none"
+        />
+        <Heading textAlign="center">Page not Found! </Heading>
+        <Text>The page you are looking for does not exist</Text>
+        <Box pt="5">
+          <Center>
+            <Link pl="2" href="/" color="blue.700" variant="ghost">
+              Home Page
+            </Link>
+            <Icon
+              as={MicroscopeIcon}
+              w={8}
+              h={8}
+              aria-label="Microscope Icon"
+              fill="none"
+            />
+          </Center>
+        </Box>
+      </Flex>
     </>
   );
 };
