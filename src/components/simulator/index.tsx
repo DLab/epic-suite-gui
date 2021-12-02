7a7b787db0f30417a72ebe790d202fed7552b0ba
Pablo Morales Pinto <70617170+ElpolMorris@users.noreply.github.com>
2021-12-02 12:52:18 -0300

    feat: add graph-view & tabs synchronized

--- src/components/simulator/index.tsx
+++ src/components/simulator/index.tsx
@@ -2,18 +2,21 @@ import { Flex } from "@chakra-ui/react";
 
 import MapResult from "../map-results/index";
 import Footer from "components/layout/Footer";
+import TabContext from "context/TabContext";
 
 import SidebarLeft from "./sidebar/SidebarLeft";
 
 const Simulator = () => {
   return (
-    <Flex maxHeight="92vh">
-      <SidebarLeft />
-      <Flex w="100%" direction="column" justify="space-between">
-        <MapResult />
-        <Footer />
+    <TabContext>
+      <Flex maxHeight="92vh">
+        <SidebarLeft />
+        <Flex w="100%" direction="column" justify="space-between">
+          <MapResult />
+          <Footer />
+        </Flex>
       </Flex>
-    </Flex>
+    </TabContext>
   );
 };
 
