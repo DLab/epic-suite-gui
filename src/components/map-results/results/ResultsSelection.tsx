00e1d9159b4394da7db52e758d993f9860946a85
gVillacura <geraldinevillacuraruz@gmail.com>
2021-12-17 14:54:44 -0300

    fix: all initial conditions checked

--- src/components/map-results/results/ResultsSelection.tsx
+++ src/components/map-results/results/ResultsSelection.tsx
@@ -1,7 +1,6 @@
 import {
   Box,
   Flex,
-  Button,
   Accordion,
   AccordionItem,
   AccordionButton,
@@ -11,7 +10,7 @@ import {
 } from "@chakra-ui/react";
 import React, { useEffect, useContext } from "react";
 
-import { GraphicsData } from "context/GraphicsContext";
+import { GraphicsData, SimulationKeysData } from "context/GraphicsContext";
 import { TabIndex } from "context/TabContext";
 
 const ResultsSelection = () => {
@@ -23,6 +22,8 @@ const ResultsSelection = () => {
     savedSimulationKeys,
     setSavedSimulationKeys,
     setSavedSimulation,
+    checkedItems,
+    setCheckedItems,
   } = useContext(GraphicsData);
 
   const model = ["S", "E", "I", "R"];
@@ -79,15 +80,144 @@ const ResultsSelection = () => {
     return savedSimulationKeys;
   };
 
+  const setEmptyStateCheckedItems = (graphicData) => {
+    const auxCheckedItems = {};
+    graphicData.forEach((simulation) => {
+      const keyList = {};
+      Object.keys(simulation).forEach((key) => {
+        if (model.includes(key)) {
+          keyList[key] = false;
+        }
+      });
+      auxCheckedItems[simulation.name] = keyList;
+    });
+
+    return auxCheckedItems;
+  };
+
+  const deleteChildChecked = (oneSimulationKeysData: SimulationKeysData) => {
+    /* delete for savedSimulationKeys */
+    const savedSimulationKeysToDelete = model.map((key) => {
+      return key + oneSimulationKeysData.name;
+    });
+
+    const newSavedSimulationKeys = savedSimulationKeys.filter(
+      (savedSimulationKey) => {
+        if (savedSimulationKeysToDelete.includes(savedSimulationKey)) {
+          return false;
+        }
+        return true;
+      }
+    );
+
+    setSavedSimulationKeys(newSavedSimulationKeys);
+
+    /* delete for savedSimulations */
+
+    const oneSavedSimulation = savedSimulation.filter(
+      (anotherSavedSimulation) => {
+        return anotherSavedSimulation.name === oneSimulationKeysData.name;
+      }
+    )[0];
+
+    const newOneSavedSimulation = {
+      name: oneSavedSimulation?.name,
+      keys: oneSavedSimulation?.keys.filter((key) => {
+        if (model.includes(key)) {
+          return false;
+        }
+        return true;
+      }),
+    };
+
+    let modifiedSimulations = savedSimulation.map((anotherSavedSimulation) => {
+      if (anotherSavedSimulation.name === newOneSavedSimulation.name) {
+        return newOneSavedSimulation;
+      }
+      return anotherSavedSimulation;
+    });
+
+    modifiedSimulations = modifiedSimulations.filter(
+      (anotherSavedSimulation) => {
+        return anotherSavedSimulation.keys.length > 0;
+      }
+    );
+
+    setSavedSimulation(modifiedSimulations);
+  };
+
+  let initialParameters = [];
+
+  const setChildChecked = (oneSimulationKeysData: SimulationKeysData) => {
+    /* save for savedSimulationKeys */
+    const savedSimulationKeysSave = model.map((key) => {
+      return key + oneSimulationKeysData.name;
+    });
+
+    const simulationKeyIsInclude = savedSimulationKeysSave.filter((key) => {
+      if (savedSimulationKeys.includes(key)) {
+        return false;
+      }
+      return true;
+    });
+
+    setSavedSimulationKeys([...savedSimulationKeys, ...simulationKeyIsInclude]);
+
+    /* save for savedSimulations */
+
+    // para verificar si ya existe una simulación con ese nombre
+
+    const isSimulationSaved = savedSimulation.filter((sim) => {
+      return oneSimulationKeysData.name === sim.name;
+    });
+
+    /* save first parameter */
+    if (isSimulationSaved.length === 0) {
+      initialParameters = [
+        ...initialParameters,
+        { name: oneSimulationKeysData.name, keys: model },
+      ];
+      setSavedSimulation([...savedSimulation, ...initialParameters]);
+    } else {
+      const parametersToSet = model.filter((modelKey) => {
+        if (isSimulationSaved[0].keys.includes(modelKey)) {
+          return false;
+        }
+        return true;
+      });
+
+      setSavedSimulation(
+        savedSimulation.map((simulation) => {
+          let simulationAux = simulation;
+          if (simulation.name === isSimulationSaved[0].name) {
+            simulationAux = {
+              name: simulation.name,
+              keys: [...simulation.keys, ...parametersToSet],
+            };
+          }
+
+          return simulationAux;
+        })
+      );
+    }
+  };
+
+  const checkParentChecked = (dataFilter: SimulationKeysData, ischecked) => {
+    if (ischecked) {
+      setChildChecked(dataFilter);
+    } else {
+      deleteChildChecked(dataFilter);
+    }
+  };
+
   useEffect(() => {
     const graphicData = responseSim ? JSON.parse(responseSim) : "";
     if (graphicData) {
       setSimulationKeys(graphicData);
-      // setInitialParameters(graphicData);
+      setCheckedItems(setEmptyStateCheckedItems(graphicData));
     }
     setSavedSimulationKeys([]);
     setSavedSimulation([]);
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [responseSim]);
 
@@ -106,43 +236,58 @@ const ResultsSelection = () => {
             </h2>
             <AccordionPanel pb={4} bg="#FFFFFF">
               <Accordion defaultIndex={[0]} allowMultiple>
-                <AccordionItem>
-                  <h2>
-                    <AccordionButton _focus={{ boxShadow: "none" }} p="2% 0">
-                      <Box flex="1" textAlign="left">
-                        Results
-                      </Box>
-                      <AccordionIcon />
-                    </AccordionButton>
-                  </h2>
-                  <AccordionPanel pb={4}>
-                    <Flex flexWrap="wrap">
-                      {Object.keys(simulation).map((key, index) => {
+                <Flex flexWrap="wrap">
+                  <Checkbox
+                    m="2% 0"
+                    onChange={(e) => {
+                      const keyList = {};
+                      Object.keys(simulation).forEach((key) => {
                         if (model.includes(key)) {
-                          return (
-                            <Checkbox
-                              size="sm"
-                              m="2% 5%"
-                              id={`${key + simulation.name}`}
-                              value={key}
-                              onChange={(e) => {
-                                saveKeys(
-                                  e.target.checked,
-                                  e.target.id,
-                                  e.target.value,
-                                  simulation.name
-                                );
-                              }}
-                            >
-                              {key}
-                            </Checkbox>
-                          );
+                          keyList[key] = e.target.checked;
                         }
-                        return false;
-                      })}
-                    </Flex>
-                  </AccordionPanel>
-                </AccordionItem>
+                      });
+                      setCheckedItems({
+                        ...checkedItems,
+                        [simulation.name]: keyList,
+                      });
+                      checkParentChecked(simulation, e.target.checked);
+                    }}
+                  >
+                    Results
+                  </Checkbox>
+                  {Object.keys(simulation).map((key) => {
+                    if (model.includes(key)) {
+                      return (
+                        <Checkbox
+                          isChecked={checkedItems[simulation.name][key]}
+                          size="sm"
+                          m="2% 5%"
+                          id={`${key + simulation.name}`}
+                          value={key}
+                          onChange={(e) => {
+                            setCheckedItems({
+                              ...checkedItems,
+                              [simulation.name]: {
+                                ...checkedItems[simulation.name],
+                                [key]: e.target.checked,
+                              },
+                            });
+
+                            saveKeys(
+                              e.target.checked,
+                              e.target.id,
+                              e.target.value,
+                              simulation.name
+                            );
+                          }}
+                        >
+                          {key}
+                        </Checkbox>
+                      );
+                    }
+                    return false;
+                  })}
+                </Flex>
                 <AccordionItem>
                   <h2>
                     <AccordionButton _focus={{ boxShadow: "none" }}>
