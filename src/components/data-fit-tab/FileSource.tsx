cbffd19f6ffaf3cd17910da982f1d5a935c3f071
gVillacura <geraldinevillacuraruz@gmail.com>
2022-04-19 18:19:21 -0400

    feat: add file data source component

--- src/components/data-fit-tab/FileSource.tsx
+++ src/components/data-fit-tab/FileSource.tsx
@@ -1,7 +1,14 @@
+import { Box, Button, Center } from "@chakra-ui/react";
 import React from "react";
 
 const FileSource = () => {
-    return <div>FileSource</div>;
+    return (
+        <Box m="0 0 3% 5%">
+            <Button size="sm" colorScheme="blue" variant="outline">
+                Upload
+            </Button>
+        </Box>
+    );
 };
 
 export default FileSource;
