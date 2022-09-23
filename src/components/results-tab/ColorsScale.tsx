9fb769345eeb7aed1efe147a84c130129a935f6c
gVillacura <geraldinevillacuraruz@gmail.com>
2022-09-23 16:46:52 -0300

    feat: get quantity indicator

--- src/components/results-tab/ColorsScale.tsx
+++ src/components/results-tab/ColorsScale.tsx
@@ -19,9 +19,26 @@ const ColorsScale = ({ maxValue }: Props) => {
     const rangeValue = Math.ceil(maxValue / colors.length);
     let min;
     let max;
+
+    const getQuantityIndicator = () => {
+        let indicator;
+        if (maxValue / 1000 > 1) {
+            indicator = 1000;
+        } else if (maxValue / 100 > 1) {
+            indicator = 100;
+        } else if (maxValue / 10 > 1) {
+            indicator = 10;
+        } else {
+            indicator = 1;
+        }
+        return indicator;
+    };
+
     return (
         <div className="info legend">
             {colors.map((color, i) => {
+                const quantityIndicator = getQuantityIndicator();
+
                 const index = colors.length - i;
                 if (i === colors.length - 1) {
                     min = 0;
@@ -30,7 +47,8 @@ const ColorsScale = ({ maxValue }: Props) => {
                     min = (index - 1) * rangeValue + 1;
                     max = index * rangeValue;
                 }
-                const minRound = Math.round(min / 1000) * 1000;
+                const minRound =
+                    Math.round(min / quantityIndicator) * quantityIndicator;
 
                 return (
                     <div key={color} style={{ textAlign: "initial" }}>
