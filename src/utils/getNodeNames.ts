0d7ca8679239d1efb402a7d2d33b0fce316b4142
Pablo Morales Pinto <70617170+ElpolMorris@users.noreply.github.com>
2023-05-09 11:16:18 -0400

    fix: seir meta custom works (#113)

--- src/utils/getNodeNames.ts
+++ src/utils/getNodeNames.ts
@@ -1,14 +1,18 @@
 import countiesData from "../data/counties.json";
 import stateData from "../data/states.json";
 
-const getNodeName = (fip, isMono) => {
-    if (isMono) {
-        return fip;
+const getNodeName = (fip, isMono, i = 0) => {
+    try {
+        if (isMono) {
+            return fip;
+        }
+        if (fip.length === 2) {
+            return stateData.data.find((state) => state[0] === fip)[2];
+        }
+        return countiesData.data.find((state) => state[5] === fip)[7];
+    } catch (error) {
+        return `${i}`;
     }
-    if (fip.length === 2) {
-        return stateData.data.find((state) => state[0] === fip)[2];
-    }
-    return countiesData.data.find((state) => state[5] === fip)[7];
 };
 
 export default getNodeName;
